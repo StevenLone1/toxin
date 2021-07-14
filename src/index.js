26f8dd5a0f7afea157b5332c5496b72e99b8b36b
StevenLone <dkulbed@gmail.com>
2021-07-14 14:12:12 +0300

    Upload all files

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './styles/styles.css'
+import './styles/scss.scss'
+// import 'material-design-iconic-font/dist/css/material-design-iconic-font.css';
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    context: path.resolve(__dirname,'src'),
+    mode: 'development',
+    entry:'./index.js',
+   
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname,'dist')
+    },
+    resolve: {
+        extensions:[]
+
+    },
+
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './index.pug',
+            filename:'[name].html'
+        }),
+        new CleanWebpackPlugin(),
+        //new CopyWebpackPlugin([
+           // {
+              //  from:path.resolve(__dirname,'src/assets'),
+              // to:path.resolve(__dirname,'dist')
+           // }
+
+        //]),
+        new MiniCssExtractPlugin(
+            {
+                filename:'[name].css'
+            }
+        ),
+    ],
+    module: {
+        rules: [
+            {
+                test:/\.pug$/,
+                use: ['pug-loader']
+            },
+            {
+                test:/\.css$/,
+                use: [MiniCssExtractPlugin.loader,'css-loader']
+            },
+            {
+                test:/\.s[ac]ss$/,
+                use: [MiniCssExtractPlugin.loader,'css-loader','sass-loader']
+            },
+            {
+                test:/\.(png|jpg|svg|gif)$/,
+                use:['file-loader']
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: ['file-loader'],
+                
+            }
+        ]
+    }
+}
